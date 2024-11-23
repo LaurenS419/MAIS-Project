b36bced3feab710c4ec30bf18eeacf4daef1ced4
LS419 <laurenspee@gmail.com>
2024-11-23 18:09:47 -0500

    admin changes

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'mp4', 'avi', 'mov', 'mkv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello World!"
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            return render_template('index.html', filename=filename)
+    return render_template('index.html', filename=None)
